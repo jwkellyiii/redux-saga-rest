e3fd90901acccfd9eafc814e4d15aab947afda1c
Zach Waggoner <zach.waggoner1@gmail.com>
2017-03-13 15:50:03 -0500

    Removed TODO.

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,6 @@ import { APIMiddlewareFactory, APINext } from './api';
 export const defaultMiddleware: APIMiddlewareFactory = () => function* (req: Request, next: APINext): any {
     const headers = req.headers || new Headers();
     if (['POST', 'PUT', 'PATCH'].includes(req.method)) {
-
-        // TODO hack; wrapping the request should happen last
         if (headers.get('Content-Type') == 'text/plain;charset=UTF-8') {
             headers.set('Content-Type', 'application/x-www-form-urlencoded');
         }
